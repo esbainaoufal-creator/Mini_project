a9a404e3b877635a8a7f755682a24413319ce80c
esbainaoufal-creator <esbainaoufal@gmail.com>
2025-09-08 11:27:32 +0200

    new update: notes count is now variable

--- mini2.c
+++ mini2.c
@@ -1,41 +1,48 @@
 #include <stdio.h>
-int main () {
+int main()
+{
     float total = 0, moyenne;
     float max, min;
-    float notes[5];
+    int n;
+    printf("how many notes you need? \n");
+    scanf("%d", &n);
+    float notes[n];
     char mention;
     int i;
-    
-    
-printf("insert 5 notes of yours : \n");
-for (i=0; i < 5; i++)
-{
 
-    printf("Note %d : ", i + 1);
-    scanf("%f", &notes[i]);
+    printf("insert notes of yours : \n");
+    for (i = 0; i < n; i++)
+    {
 
-    total += notes[i];
+        printf("Note %d : ", i + 1);
+        scanf("%f", &notes[i]);
 
-    if (i == 0)
-    max = min = notes[i];
+        total += notes[i];
 
-    
-    if (notes[i] > max) max = notes[i];
-    if (notes[i] < min) min = notes[i];
-}
-    moyenne = total / 5;
+        if (i == 0)
+            max = min = notes[i];
 
+        if (notes[i] > max)
+            max = notes[i];
+        if (notes[i] < min)
+            min = notes[i];
+    }
+    moyenne = total / n;
 
-    if (moyenne >= 90){
+    if (moyenne >= 90)
+    {
         mention = 'A';
     }
-    else if (moyenne >= 80){
+    else if (moyenne >= 80)
+    {
         mention = 'B';
     }
-    else if (moyenne >= 70){
+    else if (moyenne >= 70)
+    {
         mention = 'C';
     }
-    else if (moyenne >= 60){
+    else if (moyenne >= 60)
+    {
         mention = 'D';
     }
     else
@@ -53,10 +60,8 @@ for (i=0; i < 5; i++)
     {
         printf("Congratulations! You passed your exams succesfully \n");
     }
-    else printf("You failed. Hard luck next time! \n");
-    
-
+    else
+        printf("You failed. Hard luck next time! \n");
 
     return 0;
 }
-
